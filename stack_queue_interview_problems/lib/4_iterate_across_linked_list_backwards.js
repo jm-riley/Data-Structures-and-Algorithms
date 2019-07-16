c9a3cf0197dac6d6c970e08c33d49e81611fefc2
John-Michael Riley <johnmichael.w.riley@gmail.com>
2019-07-16 18:22:43 -0400

    add backwards singly linked list iteration

--- stack_queue_interview_problems/lib/4_iterate_across_linked_list_backwards.js
+++ stack_queue_interview_problems/lib/4_iterate_across_linked_list_backwards.js
@@ -6,11 +6,11 @@
 // Prompt:
 // -------
 //
-// Iterate over a Singly Linked List of primitives backwards. When finished, 
-// return a string representing the original linked list's values backwards 
+// Iterate over a Singly Linked List of primitives backwards. When finished,
+// return a string representing the original linked list's values backwards
 // in the following format:
 //
-//                             'A -> B -> C -> D' 
+//                             'A -> B -> C -> D'
 //
 // ------------
 // Constraints:
@@ -25,10 +25,64 @@
 // -----------
 // Let's code!
 // -----------
+class Node {
+  constructor(val) {
+    this.value = val;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.top = null;
+    this.bottom = null;
+    this.length = 0;
+  }
+
+  push(val) {
+    const newNode = new Node(val);
+    if (!this.length) {
+      this.top = newNode;
+      this.bottom = newNode;
+    } else {
+      newNode.next = this.top;
+      this.top = newNode;
+    }
+    return ++this.length;
+  }
+
+  pop() {
+    if (!this.length) return null;
+    const removedNode = this.top;
+    if (this.length === 1) {
+      this.top = null;
+      this.bottom = null;
+    } else {
+      this.top = removedNode.next;
+    }
+    this.length--;
+    return removedNode.value;
+  }
+}
 
 function iterateAcrossLinkedListBackwards(linkedList) {
-    // TODO: Implement the iterateAcrossLinkedListBackwards function here
+  // TODO: Implement the iterateAcrossLinkedListBackwards function here
+  const stack = new Stack();
+  let node = linkedList.head;
+  let result = '';
+  while (node !== null) {
+    stack.push(node);
+    node = node.next;
+  }
 
+  while (stack.length > 0) {
+    if (stack.length > 1) {
+      result += `${stack.pop().value} -> `;
+    } else {
+      result += stack.pop().value;
+    }
+  }
+  return result;
 }
 
 exports.iterateAcrossLinkedListBackwards = iterateAcrossLinkedListBackwards;
