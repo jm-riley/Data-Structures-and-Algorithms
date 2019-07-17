9eea19415bb223b74699427f8accbfde00a69e28
John-Michael Riley <johnmichael.w.riley@gmail.com>
2019-07-17 18:36:16 -0400

    add leetcode build binary tree from inorder and preorder traversal problem

--- tree_order_project/lib/leet_code_105.js
+++ tree_order_project/lib/leet_code_105.js
@@ -3,7 +3,16 @@
 
 const { TreeNode } = require('./tree_node.js');
 
-
 function buildTree(preorder, inorder) {
-
+  if (!preorder.length && !inorder.length) return null;
+  let first = preorder[0];
+  let root = new TreeNode(first);
+  let rootIdx = inorder.indexOf(first);
+  let leftInOrder = inorder.slice(0, rootIdx);
+  let rightInOrder = inorder.slice(rootIdx + 1);
+  let leftPreOrder = preorder.filter(el => leftInOrder.include(el));
+  let rightPreOrder = preorder.filter(el => rightInOrder.include(el));
+  root.left = buildTree(leftPreOrder, leftInOrder);
+  root.right = buildTree(rightPreOrder, rightInOrder);
+  return root;
 }
